5eeb80752c19e7551ed7efb6f4425d2582676732
Noa Barzelay <noabarzelay@gmail.com>
2020-04-26 13:43:45 +0300

    little touch ups

--- src/TreeList/TreeList.java
+++ src/TreeList/TreeList.java
@@ -1,7 +1,7 @@
 package TreeList;
 import TreeList.AVLTree.IAVLNode;
 
-/**
+ /**
  *
  * Tree list
  *
@@ -12,71 +12,89 @@ import TreeList.AVLTree.IAVLNode;
 	 
 	 AVLTree tree;
 	 /**
-   * public Item retrieve(int i)
-   *
-   * returns the item in the ith position if it exists in the list.
-   * otherwise, returns null
-   */
-  public Item retrieve(int i)
-  {
-	return ((AVLTree.AVLNode) tree.treeSelect(i+1)).getItem();
-  }
+     * public Item retrieve(int i)
+     *
+     * returns the item in the ith position if it exists in the list.
+     * otherwise, returns null
+     */
+	 public Item retrieve(int i)
+	 {
+		 //find node with rank i+1 (==ith position)
+		 IAVLNode node = tree.treeSelect(i+1);
+		 //if node doesn't exists
+		 if (node == null) {
+			 return null;
+		 }
+		 //return item of node
+		 else {
+			 return ((AVLTree.AVLNode) node).getItem();
+		 } 
+	 }
 
-  /**
-   * public int insert(int i, int k, String s) 
-   *
-   * inserts an item to the ith position in list with key k and info s.
-   * returns -1 if i<0 or i>n otherwise return 0.
-   */
-   public int insert(int i, int k, String s) 
-   {
-	   if (i < 0 || i > tree.size()) {
-		   return -1;
-	   }
-	   else if (i == tree.size()) {
-		   if (tree.size() == 0) {
-			   tree.setRoot(tree.new AVLNode(new Item(k, s), null, 0));
-		   }
-		   IAVLNode curNode = tree.getRoot();
-		   while (curNode.getRight() != null) {
-			   curNode = curNode.getRight();
-		   }
-		   curNode.setRight(tree.new AVLNode(new Item(k, s), curNode, 0));
-		   tree.fixTree(curNode.getParent());
-	   }
-	   else {
-		   IAVLNode curNode = tree.treeSelect(i+1);
-		   if (curNode.getLeft() == null) {
-			   curNode.setLeft(tree.new AVLNode(new Item(k, s), curNode, 0));
-		   }
-		   else {
-			   IAVLNode predecessor = ((AVLTree.AVLNode) curNode).getPredecessor();
-			   curNode.setRight(predecessor.getRight());
-			   predecessor.getRight().setParent(curNode);
-			   curNode.setParent(predecessor);
-			   predecessor.setRight(curNode);
-		   }
-		   tree.fixTree(curNode.getParent());
-	   }
-	   return 0;
-   }
+	 
+	 /**
+	  * public int insert(int i, int k, String s) 
+	  *
+	  * inserts an item to the ith position in list with key k and info s.
+	  * returns -1 if i<0 or i>n otherwise return 0.
+	  */
+	 public int insert(int i, int k, String s) 
+	 {
+		 if (i < 0 || i > tree.size()) {
+			 return -1;
+		 }
+		 //if new node insert in nth position
+		 else if (i == tree.size()) {
+			 //new node is root
+			 if (tree.size() == 0) {
+				 tree.setRoot(tree.new AVLNode(new Item(k, s), null, 0));
+			 }
+			 IAVLNode curNode = tree.getRoot();
+			 //find node in current last position and add to its right
+			 //then fix tree
+			 while (curNode.getRight() != null) {
+				 curNode = curNode.getRight();
+			 }
+			 curNode.setRight(tree.new AVLNode(new Item(k, s), curNode, 0));
+			 tree.fixTree(curNode.getParent());
+		 }
+		 else {
+			 //find node in current ith position
+			 IAVLNode curNode = tree.treeSelect(i+1);
+			 //if no left son then add new node to its left
+			 if (curNode.getLeft() == null) {
+				 curNode.setLeft(tree.new AVLNode(new Item(k, s), curNode, 0));
+			 }
+			 //else find predecessor and add to its right
+			 else {
+				 IAVLNode predecessor = ((AVLTree.AVLNode) curNode).getPredecessor();
+				 curNode.setRight(predecessor.getRight());
+				 predecessor.getRight().setParent(curNode);
+				 curNode.setParent(predecessor);
+				 predecessor.setRight(curNode);
+			 }
+			 tree.fixTree(curNode.getParent());
+		 }
+		 return 0;
+	 }
 
-  /**
-   * public int delete(int i)
-   *
-   * deletes an item in the ith position from the list.
-	* returns -1 if i<0 or i>n-1 otherwise returns 0.
-   */
-   public int delete(int i)
-   {
-	   if (i < 0 || i >= tree.size()) {
-		   return -1;
-	   }
-	   else {
-		   IAVLNode node = tree.treeSelect(i+1);
-		   tree.delete(node.getKey());
-		   return 0;
-	   } 
-   }
-	  
+	 
+	 /**
+	 * public int delete(int i)
+	 *
+	 * deletes an item in the ith position from the list.
+	 * returns -1 if i<0 or i>n-1 otherwise returns 0.
+	 */
+	 public int delete(int i)
+	 {
+		 if (i < 0 || i >= tree.size()) {
+			 return -1;
+		 }
+		 else {
+			 //find node in ith position -> delete node
+			 IAVLNode node = tree.treeSelect(i+1);
+			 tree.delete(node.getKey());
+			 return 0;
+		 } 
+	 } 
  }
